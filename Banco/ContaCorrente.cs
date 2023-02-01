4639c04eb950effa6e1c6b45b8d8f6b0112cb82e
GabrielTI20 <120021921+GabrielTI20@users.noreply.github.com>
2023-02-01 11:42:10 -0300

    ProjetoBanco

---
+++ Banco/ContaCorrente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class ContaCorrente
+{
+    public int agencia = 0;
+    public string conta = "";
+    public string nome = "";
+    public double saldo = 0.0;
+
+    public void Depositar(double valor)
+    {
+        this.saldo += valor;
+    }
+    
+    public void Sacar(double  valor) 
+    {
+        this.saldo -= valor;
+    }
+}
+
+   
---
+++ Banco/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //criando o objeto que acessa os atributos da classe
+            //Atributo parte do objeto
+            //Função  do objeto 
+           
+            ContaCorrente conta1 = new ContaCorrente();
+            ContaCorrente conta2 = new ContaCorrente();
+            ContaCorrente conta3 = new ContaCorrente();
+
+            // adicionando valor aos atributos do objeto
+
+            conta1.nome = "Gabriel";
+            conta1.agencia = 319;
+            conta1.saldo = 1920.00;
+            conta1.conta = "1237-x";
+            
+
+
+            conta2.nome = "Felipe";
+            conta2.agencia = 123;
+            conta2.saldo = 1390.00;
+            conta2.conta = "2345-x";
+           
+
+
+            conta3.nome = "Mateus";
+            conta3.agencia = 432;
+            conta3.saldo = 2100.00;
+            conta3.conta = "3114-x";
+            
+
+            //Console.WriteLine mensagem será exibida no na tela
+            Console.WriteLine(conta1.nome);
+            Console.WriteLine("\r"); //Pula uma linha
+            Console.WriteLine("saldo: R$"+ conta1.saldo);
+            Console.WriteLine("Agência: " + conta1.agencia);
+            Console.WriteLine("Conta: " + conta1.conta);
+            Console.WriteLine("\r");
+            conta1.Depositar(200); //Soma valor deposito
+            Console.WriteLine("Saldo com deposito: R$" + conta1.saldo); // Deposito mais o saldo
+            conta1.Sacar(750); //Subtrai do valor deposito
+            Console.WriteLine("Saldo atual após o saque: R$" + conta1.saldo); // Saca  mais o valor do  saldo
+            Console.WriteLine("\r");
+            Console.WriteLine("\r");
+
+
+
+            Console.WriteLine(conta2.nome);
+            Console.WriteLine("\r"); //Pula uma linha
+            Console.WriteLine("Saldo: R$" + conta2.saldo);
+            Console.WriteLine("Agêcia: " + conta2.agencia);
+            Console.WriteLine("Conta: " + conta2.conta);
+            Console.WriteLine("\r");
+            conta2.Depositar(2000); //Soma valor deposito
+            Console.WriteLine("Saldo com deposito: R$" + conta2.saldo);  // Deposito mais o saldo
+            conta2.Sacar(980); //Subtrai do valor deposito
+            Console.WriteLine("Saldo atual após o saque: R$" + conta2.saldo);// Saca  mais o valor  do saldo
+            Console.WriteLine("\r");
+            Console.WriteLine("\r");
+
+
+            Console.WriteLine(conta3.nome);
+            Console.WriteLine("\r"); //Pula uma linha
+            Console.WriteLine("Saldo: R$" + conta3.saldo);
+            Console.WriteLine("Agência: " + conta3.agencia);
+            Console.WriteLine("Conta: "+conta3.conta);
+            Console.WriteLine("\r");
+            conta3.Depositar(500); //Soma valor deposito
+            Console.WriteLine("Saldo com deposito: R$" + conta3.saldo); // Deposito mais o saldo
+            conta3.Sacar(1200); //Subtrai do valor deposito
+            Console.WriteLine("Saldo atual após o saque: R$" + conta3.saldo);// Saca  mais o valor do saldo
+            Console.WriteLine("\r");
+            Console.WriteLine("\r");
+
+        }
+    }
+}
